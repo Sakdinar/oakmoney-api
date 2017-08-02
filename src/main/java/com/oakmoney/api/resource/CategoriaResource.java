afa2ec60fcb23bba5308db3436a6ecb1ce0892ce
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-08-01 22:18:02 -0300

    Adicionado lógica para inserção de uma nova categoria

--- src/main/java/com/oakmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/oakmoney/api/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.oakmoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.oakmoney.api.model.Categoria;
 import com.oakmoney.api.repository.CategoriaRepository;
@@ -18,13 +28,28 @@ public class CategoriaResource {
 	CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> obterTodas() {
-		return categoriaRepository.findAll();
+	public ResponseEntity<List<Categoria>> obterTodas() {
+		List<Categoria> lista = categoriaRepository.findAll();
+		return null == lista || lista.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(lista);
 	}
 	
 	@GetMapping("/outro")
 	public String outro() {
 		return "ok";
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse httpResponse) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> getCategoria(@PathVariable Long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		return null == categoria  ? ResponseEntity.notFound().build() : ResponseEntity.ok(categoria);
 	}
 	
 }
