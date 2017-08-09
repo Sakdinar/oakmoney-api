2d5929097f73b13c0293ef35c27a2b1aa3c102c1
Anderson Carvalho <anderson.cvlh@gmail.com>
2017-08-09 13:05:10 -0300

    Finalizando POST de lançamento

--- src/main/java/com/oakmoney/api/resource/LancamentoResource.java
+++ src/main/java/com/oakmoney/api/resource/LancamentoResource.java
@@ -2,14 +2,20 @@ package com.oakmoney.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oakmoney.api.event.RecursoCriadoEvent;
 import com.oakmoney.api.model.Lancamento;
 import com.oakmoney.api.repository.LancamentoRepository;
 
@@ -33,8 +39,10 @@ public class LancamentoResource extends AbstractResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Lancamento> novoLancamento() {
-		return null;
+	public ResponseEntity<Lancamento> novoLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse httpResponse) {
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		getPublisher().publishEvent(new RecursoCriadoEvent(this, httpResponse, lancamento.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
 }
