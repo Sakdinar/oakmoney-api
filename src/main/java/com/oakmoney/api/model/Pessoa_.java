fa4a37e1bc759ff2132af3e46b5675b11ad5d6f7
Anderson Carvalho <anderson.cvlh@gmail.com>
2017-08-14 13:08:23 -0300

    JPA Model Generator

---
+++ src/main/java/com/oakmoney/api/model/Pessoa_.java
@@ -0,0 +1,16 @@
+package com.oakmoney.api.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Pessoa.class)
+public abstract class Pessoa_ extends com.oakmoney.api.model.AbstractModel_ {
+
+	public static volatile SingularAttribute<Pessoa, Boolean> ativo;
+	public static volatile SingularAttribute<Pessoa, Endereco> endereco;
+	public static volatile SingularAttribute<Pessoa, String> nome;
+
+}
+
