73c16e3092fbee29a57b9d059608c612b561999f
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-08-15 18:15:13 -0300

    Adicionando nome de usuário ao token

--- src/main/java/com/oakmoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/oakmoney/api/config/AuthorizationServerConfig.java
@@ -1,53 +1,58 @@
 package com.oakmoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.oakmoney.api.config.property.OakmoneyApiProperty;
+import com.oakmoney.api.config.token.CustomTokenEnhancer;
 
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
-	private OakmoneyApiProperty oakmoneyApiProperty; 
-	
+	private OakmoneyApiProperty oakmoneyApiProperty;
+
 	/**
 	 * Define credenciais do CLIENT
 	 */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-			.withClient("angular")
-			.secret("angular")
-			.scopes("read","write")
-			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(oakmoneyApiProperty.getSeguranca().getAccessTokenValiditySeconds())
-			.refreshTokenValiditySeconds(3600 * 24);
+		clients.inMemory().withClient("angular").secret("angular").scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(oakmoneyApiProperty.getSeguranca().getAccessTokenValiditySeconds())
+				.refreshTokenValiditySeconds(3600 * 24);
 	}
-	
+
 	/**
-	 * RefreshToken: Garantindo que enquanto o usuario estiver utilizando a aplicacao, o acesso nao ira expirar
+	 * RefreshToken: Garantindo que enquanto o usuario estiver utilizando a
+	 * aplicacao, o acesso nao ira expirar
 	 */
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints
-			.tokenStore(tokenStore())
-			.accessTokenConverter(accessTokenConverter())
-			.reuseRefreshTokens(false)
-			.authenticationManager(authenticationManager);
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
+		endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain).reuseRefreshTokens(false)
+				.authenticationManager(authenticationManager);
 	}
 
 	@Bean
@@ -61,6 +66,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
-	
-	
+
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomTokenEnhancer();
+	}
+
 }
