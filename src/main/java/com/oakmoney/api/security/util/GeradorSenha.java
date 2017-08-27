79421f5db9987522517163cee0dc0e803d00a2cb
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-08-27 15:31:30 -0300

    Gerado de senha na encriptação compatível

--- src/main/java/com/oakmoney/api/security/util/GeradorSenha.java
+++ src/main/java/com/oakmoney/api/security/util/GeradorSenha.java
@@ -4,9 +4,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class GeradorSenha {
 	
-	public static void main(String[] args) {
+	public static String generate(String senha) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("21092010"));
+		return encoder.encode("senha");
 	}	
 
 }
